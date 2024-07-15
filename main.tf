89fb641b09ff1f563795ec729949dfb02c44294c
francisco-ramirez-ml <Francisco.Ramirez@Xoriant.Com>
2024-07-14 17:22:52 -0700

    add prod vm

--- main.tf
+++ main.tf
@@ -37,20 +37,39 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
+# resource "aws_instance" "web" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               apt-get update
+#               apt-get install -y apache2
+#               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+#               echo "Hello World" > /var/www/html/index.html
+#               systemctl restart apache2
+#               EOF
+# }
+
+resource "aws_instance" "prod" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
 }
+
+# resource "aws_instance" "test" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.web-sg.id]
+# }
+
+# resource "aws_instance" "dev" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.web-sg.id]
+# }
+
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
