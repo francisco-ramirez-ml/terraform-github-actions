ea5402e7b87b9ecabeb323fda3244e3c544e2ffe
francisco-ramirez-ml <Francisco.Ramirez@Xoriant.Com>
2024-07-14 17:52:44 -0700

    correct quoting

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_instance" "prod" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   tags = {
-    name = 'prod - python CI/CD'
+    name = "prod - python CI/CD"
   }
 }
 
