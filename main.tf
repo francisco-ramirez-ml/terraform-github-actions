7e9db3af8b816d43604ee0ed0ba7e08b80fe3b8b
francisco-ramirez-ml <Francisco.Ramirez@Xoriant.Com>
2024-07-14 19:00:30 -0700

    add ingress rules to security group resource

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.58.0"
+      version = "4.52.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -77,6 +77,20 @@ resource "aws_instance" "prod" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
@@ -84,23 +98,6 @@ resource "aws_security_group" "web-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "ssh" {
-  security_group_id = aws_security_group.web-sg.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 22
-  ip_protocol = "tcp"
-  to_port     = 22
-}
-resource "aws_vpc_security_group_ingress_rule" "web_port" {
-  security_group_id = aws_security_group.web-sg.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 8080
-  ip_protocol = "tcp"
-  to_port     = 8080
 }
 
 
