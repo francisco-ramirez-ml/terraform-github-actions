f1e9464dafc90a892ca6916b90f3ac45d3c47c8e
francisco-ramirez-ml <Francisco.Ramirez@Xoriant.Com>
2024-07-14 18:53:52 -0700

    update aws provider version

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
+      version = "5.58.0"
     }
     random = {
       source  = "hashicorp/random"
